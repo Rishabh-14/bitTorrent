f597e8d12da1149b26ede49da0504192cf6852ce
Rishabh-14 <77384078+Rishabh-14@users.noreply.github.com>
2023-09-06 12:05:12 +0530

    correction

--- decode_list.py
+++ decode_list.py
@@ -1,30 +1,3 @@
-# Stage 1: Decode bencoded strings
-def decode_bencoded_string(encoded_str):
-    colon_pos = encoded_str.index(":")
-    length = int(encoded_str[:colon_pos])
-    decoded_str = encoded_str[colon_pos + 1 : colon_pos + 1 + length]
-    remaining = encoded_str[colon_pos + 1 + length :]
-    return decoded_str, remaining
-
-# Stage 2: Decode bencoded integers
-def decode_bencoded_integer(encoded_str):
-    if encoded_str[0] != 'i':
-        return None, encoded_str
-    e_pos = encoded_str.index('e')
-    integer_value = int(encoded_str[1:e_pos])
-    remaining = encoded_str[e_pos + 1:]
-    return integer_value, remaining
-
-# General function to decode either a string or integer
-def decode_bencoded_value(encoded_str):
-    if encoded_str[0].isdigit():
-        return decode_bencoded_string(encoded_str)
-    elif encoded_str[0] == 'i':
-        return decode_bencoded_integer(encoded_str)
-    else:
-        return None, encoded_str
-
-# Stage 3: Decode bencoded lists
 def decode_bencoded_list(encoded_str):
     if encoded_str[0] != 'l':
         return None, encoded_str
